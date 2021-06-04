4bc17c8ddde5c98e62ac2e9b89d3d24098ecc674
prattvaidya <pratt.vaidya@gmail.com>
2021-06-04 15:32:07 -0700

    add scaffold with appbar and image

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I\'m Rich.'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage("https://avatars.githubusercontent.com/u/6943596")
+        )
       ),
     ),
   );
