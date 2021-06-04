b8276f4f88f7f6e45d00f4887879a0a80eae4c2f
prattvaidya <pratt.vaidya@gmail.com>
2021-06-04 14:55:34 -0700

    first commit

---
+++ android/app/src/main/kotlin/prattvaidya/dev/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package prattvaidya.dev.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Starting point of the app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
